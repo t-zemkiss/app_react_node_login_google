41bb8f1d5d84e9461de3e12f452cc86e1441e6da
Tarek <tarek@tedybusiness.com>
2025-09-22 13:16:15 +0200

    ya podemos obtener los datos del usuario de google

---
+++ Backend/routers/google.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+require("dotenv").config();
+const { OAuth2Client } = require("google-auth-library");
+const fetch = require("node-fetch")
+
+const oAuth2Client = new OAuth2Client({
+  clientId: process.env.GOOGLE_CLIENT_ID || "",
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+  redirectUri: process.env.URL_GOOGLE_REDIRECTION || "",
+});
+
+router.get("/auth", async (req, res) => {
+  const code = req.query.code || "";
+  if (code == "") {
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+      ],
+      prompt: "consent",
+    });
+
+    console.log("Redirigiendo a Google:", authorizeUrl);
+    return res.redirect(authorizeUrl);
+  } else {
+    try {
+      const { tokens } = await oAuth2Client.getToken(code);
+      oAuth2Client.setCredentials(tokens);
+
+      console.log("Tokens de Google:", tokens);
+
+      const resUser = await fetch(
+        "https://www.googleapis.com/oauth2/v2/userinfo",
+        {
+          headers: { Authorization: `Bearer ${tokens.access_token}` },
+        }
+      );
+      const userInfo = await resUser.json();
+
+      console.log("Usuario Google:", userInfo);
+      res.json({ tokens, userInfo });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error autenticando con Google");
+    }
+  }
+});
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const body_parser = require("body-parser");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+require("dotenv").config();
 
-app = express();
-app.use("body_parser", body_parser);
+const app = express();
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.send({ Hello: "YES" });
 });
+
+const googleRouter = require("./routers/google.js");
+
+app.use("/google", googleRouter);
+
 app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
 });
